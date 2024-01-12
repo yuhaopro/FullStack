fb25e04f624d9ab97e9027f44650543c149e5baa
yuhaopro <yuhao9583@gmail.com>
2024-01-12 10:29:07 +0800

    refactor: 6.15

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,13 +2,15 @@ import { useDispatch } from "react-redux";
 import AnecdoteFilter from "./components/AnecdoteFilter";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
-import Notification from "./components/Notification"
+import Notification from "./components/Notification";
 import { useEffect } from "react";
+import api from "../services/api";
+import { setAnecdote } from "./reducers/anecdoteReducer";
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    
-  }, [])
+    api.getAll().then((res) => dispatch(setAnecdote(res)));
+  }, []);
   return (
     <div>
       <Notification></Notification>
