979bcadbb3b6354703a09072801faa0e2e876182
yuhaopro <yuhao9583@gmail.com>
2024-01-13 20:47:22 +0800

    feat: 7.7

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    // make a call to the api endpoint with the search result
+    const searchCountry = async () => {
+      try {
+        const response = await axios.get(
+          `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`
+        );
+        setCountry({ ...response, found: true });
+        console.log({ ...response, found: true });
+      } catch (error) {
+        setCountry(error);
+      }
+    };
+    searchCountry();
+  }, [name]);
+
+  return country;
+};
+
+const Country = ({ country }) => {
+  console.log("Country", country);
+  if (!country) {
+    return null;
+  }
+
+  if (!country.found) {
+    console.log("Country.found", country.found);
+    return <div>not found...</div>;
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flags.png}
+        height="100"
+        alt={`${country.data.flags.alt}`}
+      />
+    </div>
+  );
+};
+
+const App = () => {
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
+
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  );
+};
+
+export default App;
